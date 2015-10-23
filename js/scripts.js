09253d5412ca6ed4d8fcb1beb6f2f7d1f2369df8
Midori <Midori@kaworus-mbp.hsd1.or.comcast.net>
2015-10-23 12:49:56 -0700

    Add spec that writes the last two lines of the song before the ending line.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
 var roundOne = function(number) {
   var numberLower = number - 1;
-  return number + " bottles of beer on the wall, " + number + " bottles of beer! " + "Take one down, pass it around, " + numberLower + " bottles of beer on the wall!";
+  return number + " bottles of beer on the wall, " + number + " bottles of beer! " + "Take one down, pass it around, " + numberLower + " bottles of beer on the wall! ";
 };
-// var inputNumber = function(number) {
-//   var numberLower = function(lower) {
-//     return number + " bottles of beer on the wall, " + number + " bottles of beer. " + "Take one down, pass it around, " + lower + " bottles of beer on the wall!";
-//   };
-// };
-// var beerCountdown = function(number) {
-//   return "3 bottles of beer on the wall, 3 bottles of beer. Take one down, pass it around, 2 bottles of beer on the wall! 2 bottles of beer on the wall, 2 bottles of beer. Take one down, pass it around, 1 bottle of beer on the wall! 1 bottle of beer on the wall, 1 bottle of beer. Take one down, pass it around, 0 bottles of beer on the wall! No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 3 bottles of beer on the wall!";
-// }
+var nextRounds = function(number) {
+  var oneBottle = 1;
+  if (number > 2) {
+    return roundOne(number) + nextRounds(number-1);
+  } else if (number <= 2) {
+    return number + " bottles of beer on the wall, " + number + " bottles of beer! " + "Take one down pass it around, " + oneBottle + " bottle of beer on the wall! "
+  } else {
+    return "";
+  }
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,18 @@
 describe('roundOne', function() {
   it("returns the first line of the song with the number input at the the 1st and 2nd position", function() {
-    expect(roundOne(3)).to.equal("3 bottles of beer on the wall, 3 bottles of beer! Take one down, pass it around, 2 bottles of beer on the wall!");
+    expect(roundOne(5)).to.equal("5 bottles of beer on the wall, 5 bottles of beer! Take one down, pass it around, 4 bottles of beer on the wall! ");
   });
   it("returns the first line of the song with the next lower number in the 3rd position", function() {
-    expect(roundOne(3)).to.equal("3 bottles of beer on the wall, 3 bottles of beer! Take one down, pass it around, 2 bottles of beer on the wall!");
+    expect(roundOne(5)).to.equal("5 bottles of beer on the wall, 5 bottles of beer! Take one down, pass it around, 4 bottles of beer on the wall! ");
   });
+});
+describe('nextRounds', function() {
+  it("returns the all except the last two lines of the song with the numbers going lower using a loop", function() {
+    expect(nextRounds(5)).to.equal("5 bottles of beer on the wall, 5 bottles of beer! Take one down, pass it around, 4 bottles of beer on the wall! 4 bottles of beer on the wall, 4 bottles of beer! Take one down, pass it around, 3 bottles of beer on the wall! 3 bottles of beer on the wall, 3 bottles of beer! Take one down, pass it around, 2 bottles of beer on the wall! 2 bottles of beer on the wall, 2 bottles of beer! Take one down pass it around, 1 bottle of beer on the wall! ");
+  });
+  it("returns the last two lines of the song, before the ending line, using singular bottle instead of bottles", function() {
+    expect(nextRounds(5)).to.equal("5 bottles of beer on the wall, 5 bottles of beer! Take one down, pass it around, 4 bottles of beer on the wall! 4 bottles of beer on the wall, 4 bottles of beer! Take one down, pass it around, 3 bottles of beer on the wall! 3 bottles of beer on the wall, 3 bottles of beer! Take one down, pass it around, 2 bottles of beer on the wall! 2 bottles of beer on the wall, 2 bottles of beer! Take one down pass it around, 1 bottle of beer on the wall! " )
+  })
 });
 // describe('numberLower', function() {
 //   it("returns the first line of the song with the next lower number at the end", function() {
