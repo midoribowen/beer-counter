6fe6e8a793c500f2310b3343c680d2d1b823131f
Midori <Midori@kaworus-mbp.hsd1.or.comcast.net>
2015-10-23 11:38:34 -0700

    Re-do first spec for var roundOne.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
-var beerCountdown = function(number) {
-  return number + " bottles of beer on the wall, " + number + " bottles of beer. " + "Take one down, pass it around, " + number + " bottles of beer on the wall!";
+var roundOne = function(number) {
+  return number + " bottles of beer on the wall, " + number + " bottles of beer! " + "Take one down, pass it around, " + number + " bottles of beer on the wall!";
 };
+// var inputNumber = function(number) {
+//   var numberLower = function(lower) {
+//     return number + " bottles of beer on the wall, " + number + " bottles of beer. " + "Take one down, pass it around, " + lower + " bottles of beer on the wall!";
+//   };
+// };
+// var beerCountdown = function(number) {
+//   return "3 bottles of beer on the wall, 3 bottles of beer. Take one down, pass it around, 2 bottles of beer on the wall! 2 bottles of beer on the wall, 2 bottles of beer. Take one down, pass it around, 1 bottle of beer on the wall! 1 bottle of beer on the wall, 1 bottle of beer. Take one down, pass it around, 0 bottles of beer on the wall! No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 3 bottles of beer on the wall!";
+// }
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,15 @@
-describe('beerCountdown', function() {
-  it("returns the first line of the song with the number input inserted where it goes", function() {
-    expect(beerCountdown(99)).to.equal("99 bottles of beer on the wall, 99 bottles of beer. Take one down, pass it around, 99 bottles of beer on the wall!");
+describe('roundOne', function() {
+  it("returns the first line of the song with the number input at the the 1st and 2nd position", function() {
+    expect(roundOne(3)).to.equal("3 bottles of beer on the wall, 3 bottles of beer! Take one down, pass it around, 3 bottles of beer on the wall!");
   });
 });
+// describe('numberLower', function() {
+//   it("returns the first line of the song with the next lower number at the end", function() {
+//     expect(numberLower(3)).to.equal("3 bottles of beer on the wall, 3 bottles of beer. Take one down, pass it around, 2 bottles of beer on the wall!");
+//   });
+// });
+// describe('beerCountdown', function() {
+//   it("returns the whole song 99 Bottles of Beer on the wall starting at the number input by the user", function() {
+//     expect(beerCountdown(3)).to.equal("3 bottles of beer on the wall, 3 bottles of beer. Take one down, pass it around, 2 bottles of beer on the wall! 2 bottles of beer on the wall, 2 bottles of beer. Take one down, pass it around, 1 bottle of beer on the wall! 1 bottle of beer on the wall, 1 bottle of beer. Take one down, pass it around, 0 bottles of beer on the wall! No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 3 bottles of beer on the wall!");
+//   });
+// });
